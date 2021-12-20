617e014785da77e83ba749d5bb5d789ecb571f8c
Nikolay Tomov <nikolay.tomov@nexo.io>
2021-12-20 08:59:33 +0200

    add logic for adding/removing some room plus logic for booking room

--- back-end/src/main/java/com/zealroom/room/booking/system/repositories/BookingRepository.java
+++ back-end/src/main/java/com/zealroom/room/booking/system/repositories/BookingRepository.java
@@ -17,5 +17,6 @@ public interface BookingRepository extends JpaRepository<Booking, String> {
     @Query("select r from Room r JOIN Booking b where b.checkIn >= ?1 and b.checkOut <= ?1")
     List<Room> findByCheckInAndCheckOut(LocalDate checkIn, LocalDate checkOut);
 
-
+    @Query("select b from Booking b where b.room = ?1 AND ((b.checkIn<?1 AND b.checkOut>?1) OR (b.checkIn<?2 AND b.checkOut>?2))")
+    List<Booking> findAllByRoomAndCheckInCheckOut(Room room, LocalDate checkIn, LocalDate checkout);
 }
