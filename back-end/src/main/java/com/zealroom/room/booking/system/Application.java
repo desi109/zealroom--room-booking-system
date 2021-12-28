73fc6c4d4730660518d122097f706386544d6d78
Desislava Milusheva <demilusheva@gmail.com>
2021-12-28 20:26:10 +0200

    Fix cors policy

--- back-end/src/main/java/com/zealroom/room/booking/system/Application.java
+++ back-end/src/main/java/com/zealroom/room/booking/system/Application.java
@@ -3,11 +3,11 @@ package com.zealroom.room.booking.system;
 import com.zealroom.room.booking.system.repositories.UserRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 @EnableJpaRepositories(basePackageClasses = UserRepository.class)
@@ -15,6 +15,16 @@ public class Application {
 
   public static void main(String[] args) {
     SpringApplication.run(Application.class, args);
+  }
+
+  @Bean
+  public WebMvcConfigurer corsConfigurer() {
+    return new WebMvcConfigurer() {
+      @Override
+      public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedOrigins("*");
+      }
+    };
   }
 }
 
